ba1d9394815ee5a8ba7b2645708a4886a4c91496
Madhu Srinivasan <msrinivasa@cray.com>
2020-10-21 18:17:33 -0500

    finessing our reports

--- slurm/gen_report.sh
+++ slurm/gen_report.sh
@@ -11,7 +11,7 @@
 #SBATCH --error=gen_report.%J.err
 # maximum job time in HH:MM:SS
 #SBATCH --time=02:00:00
-#SBATCH --nodes=2
+#SBATCH --nodes=11
 # maximum memory
 #SBATCH --mem=128000
 # required CPUs per task
@@ -23,44 +23,27 @@ module load perftools-base
 module load perftools
 
 srun --exclusive -N 1 -n 1 /lus/cls01053/msrinivasa/develop/lammps/perftools/genreport.sh \
-     /lus/cls01053/msrinivasa/develop/lammps/slurm/experiment.014/data/congested/lmp+tracing+73891-1000t &
+     /lus/cls01053/msrinivasa/develop/lammps/slurm/experiment.012/data/idle1/lmp+tracing+104200-1000t/ &
 srun --exclusive -N 1 -n 1 /lus/cls01053/msrinivasa/develop/lammps/perftools/genreport.sh \
-     /lus/cls01053/msrinivasa/develop/lammps/slurm/experiment.014/data/idle/lmp+tracing+72741-1000t
-
-
-
-# srun --exclusive -N 1 -n 1 /lus/cls01053/msrinivasa/develop/lammps/perftools/genreport.sh \
-#      /lus/cls01053/msrinivasa/develop/lammps/slurm/experiment.005/data/idle/lmp+tracing+235468-1005t &
-# srun --exclusive -N 1 -n 1 /lus/cls01053/msrinivasa/develop/lammps/perftools/genreport.sh \
-#      /lus/cls01053/msrinivasa/develop/lammps/slurm/experiment.005/data/congested/lmp+tracing+236174-1005t &
-# srun --exclusive -N 1 -n 1 /lus/cls01053/msrinivasa/develop/lammps/perftools/genreport.sh \
-#      /lus/cls01053/msrinivasa/develop/lammps/slurm/experiment.006/data/idle/lmp+tracing+237621-1005t &
-# srun --exclusive -N 1 -n 1 /lus/cls01053/msrinivasa/develop/lammps/perftools/genreport.sh \
-#      /lus/cls01053/msrinivasa/develop/lammps/slurm/experiment.006/data/congested/lmp+tracing+238345-1005t &
-# srun --exclusive -N 1 -n 1 /lus/cls01053/msrinivasa/develop/lammps/perftools/genreport.sh \
-#      /lus/cls01053/msrinivasa/develop/lammps/slurm/experiment.007/data/idle/lmp+tracing+240216-1005t &
-# srun --exclusive -N 1 -n 1 /lus/cls01053/msrinivasa/develop/lammps/perftools/genreport.sh \
-#      /lus/cls01053/msrinivasa/develop/lammps/slurm/experiment.007/data/congested/lmp+tracing+241022-1005t &
-# srun --exclusive -N 1 -n 1 /lus/cls01053/msrinivasa/develop/lammps/perftools/genreport.sh \
-#      /lus/cls01053/msrinivasa/develop/lammps/slurm/experiment.008/data/idle/lmp+tracing+211815-1000t &
-# srun --exclusive -N 1 -n 1 /lus/cls01053/msrinivasa/develop/lammps/perftools/genreport.sh \
-#      /lus/cls01053/msrinivasa/develop/lammps/slurm/experiment.008/data/congested/lmp+tracing+212856-1000t &
-# srun --exclusive -N 1 -n 1 /lus/cls01053/msrinivasa/develop/lammps/perftools/genreport.sh \
-#      /lus/cls01053/msrinivasa/develop/lammps/slurm/experiment.009/data/idle/lmp+tracing+214220-1000t &
-# srun --exclusive -N 1 -n 1 /lus/cls01053/msrinivasa/develop/lammps/perftools/genreport.sh \
-#      /lus/cls01053/msrinivasa/develop/lammps/slurm/experiment.009/data/congested/lmp+tracing+215485-1000t &
-# srun --exclusive -N 1 -n 1 /lus/cls01053/msrinivasa/develop/lammps/perftools/genreport.sh \
-#      /lus/cls01053/msrinivasa/develop/lammps/slurm/experiment.010/data/idle/lmp+tracing+216945-1000t &
-# srun --exclusive -N 1 -n 1 /lus/cls01053/msrinivasa/develop/lammps/perftools/genreport.sh \
-#      /lus/cls01053/msrinivasa/develop/lammps/slurm/experiment.010/data/congested/lmp+tracing+218486-1000t &
-# srun --exclusive -N 1 -n 1 /lus/cls01053/msrinivasa/develop/lammps/perftools/genreport.sh \
-#      /lus/cls01053/msrinivasa/develop/lammps/slurm/experiment.011/data/idle/lmp+tracing+220520-1000t &
-# srun --exclusive -N 1 -n 1 /lus/cls01053/msrinivasa/develop/lammps/perftools/genreport.sh \
-#      /lus/cls01053/msrinivasa/develop/lammps/slurm/experiment.011/data/congested/lmp+tracing+221981-1000t &
-# srun --exclusive -N 1 -n 1 /lus/cls01053/msrinivasa/develop/lammps/perftools/genreport.sh \
-#      /lus/cls01053/msrinivasa/develop/lammps/slurm/experiment.011.2/data/idle/lmp+tracing+236918-1000t &
-# srun --exclusive -N 1 -n 1 /lus/cls01053/msrinivasa/develop/lammps/perftools/genreport.sh \
-#      /lus/cls01053/msrinivasa/develop/lammps/slurm/experiment.011.2/data/congested/lmp+tracing+237774-1000t 
+     /lus/cls01053/msrinivasa/develop/lammps/slurm/experiment.012/data/idle2/lmp+tracing+104655-1000t/ &
+srun --exclusive -N 1 -n 1 /lus/cls01053/msrinivasa/develop/lammps/perftools/genreport.sh \
+     /lus/cls01053/msrinivasa/develop/lammps/slurm/experiment.016/data/idle1/lmp+tracing+105768-1000t/ &
+srun --exclusive -N 1 -n 1 /lus/cls01053/msrinivasa/develop/lammps/perftools/genreport.sh \
+     /lus/cls01053/msrinivasa/develop/lammps/slurm/experiment.016/data/idle2/lmp+tracing+106010-1000t/ &
+srun --exclusive -N 1 -n 1 /lus/cls01053/msrinivasa/develop/lammps/perftools/genreport.sh \
+     /lus/cls01053/msrinivasa/develop/lammps/slurm/experiment.017/data/idle/lmp+tracing+108175-1000t/ &
+srun --exclusive -N 1 -n 1 /lus/cls01053/msrinivasa/develop/lammps/perftools/genreport.sh \
+     /lus/cls01053/msrinivasa/develop/lammps/slurm/experiment.017/data/congested/lmp+tracing+109952-1000t/ &
+srun --exclusive -N 1 -n 1 /lus/cls01053/msrinivasa/develop/lammps/perftools/genreport.sh \
+     /lus/cls01053/msrinivasa/develop/lammps/slurm/experiment.018/data/idle/lmp+tracing+112992-1000t/ &
+srun --exclusive -N 1 -n 1 /lus/cls01053/msrinivasa/develop/lammps/perftools/genreport.sh \
+     /lus/cls01053/msrinivasa/develop/lammps/slurm/experiment.018/data/congested/lmp+tracing+113973-1000t/ &
+srun --exclusive -N 1 -n 1 /lus/cls01053/msrinivasa/develop/lammps/perftools/genreport.sh \
+     /lus/cls01053/msrinivasa/develop/lammps/slurm/experiment.019/data/idle1/lmp+tracing+116723-1000t/ &
+srun --exclusive -N 1 -n 1 /lus/cls01053/msrinivasa/develop/lammps/perftools/genreport.sh \
+     /lus/cls01053/msrinivasa/develop/lammps/slurm/experiment.019/data/idle2/lmp+tracing+117204-1000t/ &
+srun --exclusive -N 1 -n 1 /lus/cls01053/msrinivasa/develop/lammps/perftools/genreport.sh \
+     /lus/cls01053/msrinivasa/develop/lammps/slurm/experiment.019/data/congested/lmp+tracing+118309-1000t/
 
 # wait until all job steps complete
 wait 
