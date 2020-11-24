1941705469c1386dbbb6214245f8e11208a450fa
Madhu Srinivasan <msrinivasa@cray.com>
2020-11-23 18:00:26 -0600

    added new slurm scripts

--- slurm/gpc_lammps.sh
+++ slurm/gpc_lammps.sh
@@ -2,20 +2,19 @@
 
 ###
 # job name
-#SBATCH --job-name=gpc_lammps.002
+#SBATCH --job-name=E0001
 # specify its partition
 #SBATCH --partition=workq
 # job stdout file
-#SBATCH --output=gpc_lammps.002.%J.out
+#SBATCH --output=E0001.%J.out
 # job stderr file
-#SBATCH --error=gpc_lammps.002.%J.err
+#SBATCH --error=E0001.%J.err
 # maximum job time in HH:MM:SS
 #SBATCH --time=06:00:00
-#SBATCH --nodes=500
+#SBATCH --nodes=100
 # maximum memory
-#SBATCH --mem-per-cpu=512
+#SBATCH --mem-per-cpu=512M
 # run a single task
-#SBATCH --cpus-per-task=1
 ###
 
 module restore PrgEnv-cray
@@ -23,69 +22,78 @@ module load perftools-base
 module load perftools
 
 # Define experiment name
-export EXPERIMENT_NAME=experiment.002
+export EXPERIMENT_NAME=$SLURM_JOB_NAME
 
 # Define allocations
-export TOTAL_NC=500
+export TOTAL_NC=$SLURM_JOB_NUM_NODES
 export LAMMPS_NC=50
-export LAMMPS_PE_COUNT=200
-export GPCNET_NC=450
-export GPCNET_PE_COUNT=4500
+export LAMMPS_PPN=4
+export GPCNET_NC=50
+export GPCNET_PPN=10
 
-# Define directories
-export APP_BASE_DIR=/lus/cls01053/msrinivasa/develop
+# Define directories and files
+export APP_BASE_DIR=/home/users/msrinivasa/develop
 export DATA_DIR=./data
 export EXPERIMENT_METAFILE=$EXPERIMENT_NAME.README.txt
+export EXPERIMENT_JOBFILE=$EXPERIMENT_NAME.JOBFILE.csv
 mkdir -p $DATA_DIR
 
 # Write metadata into a README file for the experiment
 echo $EXPERIMENT_NAME>$EXPERIMENT_METAFILE
-echo "GPCNet Allocation: 90%, 90 nodes, 10 PPN">>$EXPERIMENT_METAFILE
-echo "LAMMPS Allocation: 10%, 10 nodes, 4 PPN">>$EXPERIMENT_METAFILE
-
+echo "Total Allocation: "$TOTAL_NC>>$EXPERIMENT_METAFILE
+echo "GPCNet Allocation: "$GPCNET_NC>>$EXPERIMENT_METAFILE
+echo "LAMMPS Allocation: "$LAMMPS_NC>>$EXPERIMENT_METAFILE
+echo "Nodelist: "$SLURM_JOB_NODELIST>>$EXPERIMENT_METAFILE
 # Define a few craypat related environment variables for this jobstep
 export PAT_RT_EXPDIR_BASE=$DATA_DIR/idle
-export PAT_RT_EXPFILE_MAX=$LAMMPS_PE_COUNT
+# export PAT_RT_EXPFILE_MAX=$LAMMPS_PE_COUNT
 mkdir -p $PAT_RT_EXPDIR_BASE
 
-echo "Idle run starttime: "`date -uI'seconds'`>>$EXPERIMENT_METAFILE
+# Record the job start time
+export LAMMPS_IDLE_START=`date -uI'seconds'`
 # Run lammps without congestion with 4 ppn
-srun --exclusive -N $LAMMPS_NC -n $LAMMPS_PE_COUNT \
+srun --exclusive -N $LAMMPS_NC -n $LAMMPS_PPN \
     $APP_BASE_DIR/lammps/build/lmp+tracing \
     -i $APP_BASE_DIR/lammps/examples/DIFFUSE/in.msd.2d \
     > $PAT_RT_EXPDIR_BASE/lammps.idle.out
-echo "Idle run endtime: "`date -uI'seconds'`>>$EXPERIMENT_METAFILE
+export LAMMPS_IDLE_END=`date -uI'seconds'`
 
 # wait for 30 seconds
 sleep 30
 
 # Define a few craypat related environment variables for this jobstep
 export PAT_RT_EXPDIR_BASE=$DATA_DIR/congested
-export PAT_RT_EXPFILE_MAX=$LAMMPS_PE_COUNT
+# export PAT_RT_EXPFILE_MAX=$LAMMPS_PE_COUNT
 mkdir -p $PAT_RT_EXPDIR_BASE
 
-
-echo "GPCNET run starttime: "`date -uI'seconds'`>>$EXPERIMENT_METAFILE
+export GPCNET_START=`date -uI'seconds'`
 # Run gpcnet on its allocation with 10 ppn
-srun --exclusive -N $GPCNET_NC  -n $GPCNET_PE_COUNT \
+srun --exclusive -N $GPCNET_NC  -n $GPCNET_PPN \
      $APP_BASE_DIR/GPCNET/network_load_test > $PAT_RT_EXPDIR_BASE/gpcnet.out &
 
 # wait for a minute till gpcnet primes up the network
 sleep 30
 
-
-echo "Congested LAMMPS run starttime: "`date -uI'seconds'`>>$EXPERIMENT_METAFILE
+export LAMMPS_CONGESTED_START=`date -uI'seconds'`
 # Run lammps with congestion with 4 ppn
 srun --exclusive -N $LAMMPS_NC -n $LAMMPS_PE_COUNT \
      $APP_BASE_DIR/lammps/build/lmp+tracing \
      -i $APP_BASE_DIR/lammps/examples/DIFFUSE/in.msd.2d \
-    > $PAT_RT_EXPDIR_BASE/lammps.congested.out
-echo "Congested LAMMPS run endtime: "`date -uI'seconds'`>>$EXPERIMENT_METAFILE
+     > $PAT_RT_EXPDIR_BASE/lammps.congested.out
+export LAMMPS_CONGESTED_END=`date -uI'seconds'`
 
 # wait till all jobsteps finish
 wait
+
 # now we know GPCNET is done
-echo "GPCNET run endtime: "`date -uI'seconds'`>>$EXPERIMENT_METAFILE
+export GPCNET_END=`date -uI'seconds'`
+
+# record all jobsteps
+echo "start_time,end_time,job_id,job_name,user"
+echo $LAMMPS_IDLE_START,$LAMMPS_IDLE_END,$EXPERIMENT_NAME.01,$EXPERIMENT_NAME.LAMMPS.IDLE,$USER
+echo $GPCNET_START,$GPCNET_END,$EXPERIMENT_NAME.02,$EXPERIMENT_NAME.GPCNET.AGGRESSOR,$USER
+echo $LAMMPS_CONGESTED_START,$LAMMPS_CONGESTED_END,$EXPERIMENT_NAME.03,$EXPERIMENT_NAME.LAMMPS.CONGESTED,$USER
+
 
 # Clean up GPCNET output, since we don't need it
 rm *.dat
@@ -93,8 +101,6 @@ rm *.rec
 
 # Clean up the lammps log output (it is redundant)
 rm log.lammps
-
-
 
 # sbatch cmd on Shandy that works [October 6, 2020]
 
